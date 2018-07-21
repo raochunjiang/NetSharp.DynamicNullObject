4bfff0077ec69d85927367b0c809c15aae35d006
raochunjiang <caterpillar.com@outlook.com>
2018-07-22 02:09:24 +0800

    重构以支持多程序集

---
+++ src/NetSharp.DynamicNullObject.Tests/Models/MyServiceWithNonePublicConstructors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetSharp.DynamicNullObject.Tests.Models
+{
+    public class MyServiceWithNonePublicConstructors
+    {
+        private MyServiceWithNonePublicConstructors()
+        {
+
+        }
+    }
+}
